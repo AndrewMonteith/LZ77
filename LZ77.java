7184ecb574f1fae22af1bc4b5b4ff66fc187b788
AndrewMonteith <andrewmonteith1999@gmail.com>
2019-01-28 15:26:42 +0000

    fixed a few java bugs

--- LZ77.java
+++ LZ77.java
@@ -1,9 +1,11 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 final class FatIndex {
     public final int index;
@@ -17,10 +19,10 @@ final class FatIndex {
 
 final class LZ77Triple {
     public final int d;
-    public final int l;
+    public final short l;
     public final byte next;
 
-    public LZ77Triple(int d, int l, byte next) {
+    public LZ77Triple(int d, short l, byte next) {
         this.d = d;
         this.l = l;
         this.next = next;
@@ -34,26 +36,34 @@ final class LZ77Triple {
 public class LZ77 {
     private final int window, lookahead;
 
+    private static final byte NO_SYMBOL = -1;
+
+    /**
+     * Decodes an LZ77 encoded message.
+     * 
+     * @param triples List to decode
+     * @return Decoded message
+     */
     public static byte[] decode(List<LZ77Triple> triples) {
         var bufferSize = triples.stream().mapToInt(s -> s.l + (s.next == -1 ? 0 : 1)).sum();
-        var result = new byte[bufferSize];
+        var buffer = new byte[bufferSize];
         var ptr = 0;
 
         for (var triple : triples) {
             if (triple.l > 0) {
                 var end = ptr - triple.d + triple.l;
                 for (var i = ptr - triple.d; i < end; ++i, ++ptr) {
-                    result[ptr] = result[i];
+                    buffer[ptr] = buffer[i];
                 }
             }
 
-            if (triple.next != -1) {
-                result[ptr] = triple.next;
+            if (triple.next != NO_SYMBOL) {
+                buffer[ptr] = triple.next;
                 ptr += 1;
             }
         }
 
-        return result;
+        return buffer;
     }
 
     private boolean doSequencesMatch(byte[] symbols, int index1, int index2, int length) {
@@ -101,6 +111,12 @@ public class LZ77 {
         return new FatIndex(longestPrefixI, longestPrefixLen);
     }
 
+    /**
+     * Encodes an array of symbols
+     * 
+     * @param symbols symbols to encode
+     * @return encoded symbols
+     */
     public List<LZ77Triple> encode(byte[] symbols) {
         var result = new ArrayList<LZ77Triple>();
 
@@ -108,14 +124,13 @@ public class LZ77 {
             var prefix = findLongestPrefixInWindow(symbols, symbolI);
 
             if (prefix.index == symbolI) {
-                result.add(new LZ77Triple(0, 0, symbols[symbolI]));
+                result.add(new LZ77Triple(0, (byte) 0, symbols[symbolI]));
             } else {
                 var howFarBack = symbolI - prefix.index;
-
                 var nextSymbolI = symbolI + prefix.len;
                 var nextSymbol = (nextSymbolI < symbols.length) ? symbols[nextSymbolI] : -1;
 
-                result.add(new LZ77Triple(howFarBack, prefix.len, nextSymbol));
+                result.add(new LZ77Triple(howFarBack, (short) prefix.len, nextSymbol));
                 symbolI += prefix.len;
             }
         }
@@ -123,41 +138,94 @@ public class LZ77 {
         return result;
     }
 
+    /**
+     * Encodes a given string
+     * 
+     * @param s string to encode
+     * @return encoded string
+     */
     public List<LZ77Triple> encode(String s) {
         return encode(s.getBytes());
     }
 
-    public List<LZ77Triple> encode(Path path) throws java.io.IOException {
-        return encode(Files.readAllBytes(path));
-    }
-
+    /**
+     * Create LZ77 with window size 65535 and lookahead size 255
+     */
     public LZ77() {
         this(65535, 255);
     }
 
+    /**
+     * Create new LZ77 coder with specific window and lookahead sizes.
+     */
     public LZ77(int window, int lookahead) {
         this.window = window;
         this.lookahead = lookahead;
     }
 
-    public static boolean checkString(String s) {
-        var coder = new LZ77();
+    // ------------------- Experimentation Logic.
 
-        var encoded = coder.encode(s);
-        var decoded = LZ77.decode(encoded);
+    static final class TimedResult<T> {
+        public final T result;
+        public final double duration;
 
-        return (new String(decoded)).equals(s);
+        public TimedResult(T result, double duration) {
+            this.result = result;
+            this.duration = duration;
+        }
     }
 
-    public static void encodeFile(String fileName) throws java.io.IOException {
+    private static <T> TimedResult<T> time(Supplier<T> f) {
+        var now = System.nanoTime();
+        var result = f.get();
+        return new TimedResult<T>(result, (System.nanoTime() - now) / 1_000_000_000.0);
+    }
+
+    private static byte[] generateRandomSymbols(long length) {
+        byte[] array = new byte[(int) length];
+        new java.util.Random().nextBytes(array);
+
+        // make sure there are no special symbols in there
+        // which we assume aren't part of our messages alphabet
+        for (var i = 0; i < array.length; ++i) {
+            if (array[i] == NO_SYMBOL) {
+                array[i] = 0;
+            }
+        }
+        return array;
+    }
+
+    private static void testByteData(String id, byte[] data) {
         var coder = new LZ77();
 
-        var result = coder.encode(Paths.get("./test_data", fileName));
+        System.out.println("------------ For " + id);
+        var encoded = time(() -> coder.encode(data));
+        var decoded = time(() -> LZ77.decode(encoded.result));
 
-        System.out.println("Encoded with " + result.size() + " triples");
+        System.out.printf("Encoded in %.3f seconds\n", encoded.duration);
+        System.out.printf("Decoded in %.3f seconds\n", decoded.duration);
+
+        var originalSize = data.length;
+        var codedSize = encoded.result.size() * 6;
+
+        System.out.printf("Before Compression was %d bytes, after compression %d bytes. Compression Ration %.3f\n",
+                originalSize, codedSize, 100 * (1 - (double) codedSize / originalSize));
+    }
+
+    private static void generateExperimentationDataForStructured() throws IOException {
+        File[] structuredFiles = (new File("./test_data/").listFiles());
+
+        for (var file : structuredFiles) {
+            var fileContents = Files.readAllBytes(file.toPath());
+
+            testByteData(file.getName(), fileContents);
+
+            var unstrucutredEquivalent = generateRandomSymbols(file.length());
+            testByteData(file.length() + " - unstructured", unstrucutredEquivalent);
+        }
     }
 
     public static void main(String[] args) throws IOException {
-
+        generateExperimentationDataForStructured();
     }
 }
--- experimentation.md
+++ experimentation.md
@@ -8,56 +8,25 @@ Compare running time on the following things
     - Use a corpus for the strutucal redundant strings
 2. Different parameter combinations
     - on structurally redundant strings
+    - parameter ranges for W=[2^i for i \in [1, 16]], L = [2^i for i in [3, 10]]
+    - find parameters mentioned in original paper?
 3. Various Media Formats - these might contains other redundancies like frequency (do this one is 1/2 doesn't pad out like 2/3-1 page)
+    - hyptothesis it would be wank on images, test on images, show it is wank on images (especially JPEG!)
 
-Can we define some kind of notion of best running time? Maybe (Time Taken / Compression Ratio)
 
 ## 2. Runtime of the Decoder
 
 1. High Structural Redundancy vs Low Structural Redundacy
-    - Do higly compressed files take longer to decode than not very compressed files.
+    - Do compressed files take longer to decode than not very compressed files.
 2. Perhaps some paramater combinations really slowed down the decoding?
     - larger window sizes => largest prefix compression => larger decoding time?
+    - look for files that were compressed really well in 3
 
 ## 3. Compression Ratio
 
 Same 1, 2, 3 stuff. Althought "general purpose"
 perhaps some media formats will favour better, i imagine it would be shit on images but nice on text
 so could claim it's not terribly general purpose.
-
-# All This shit is wrong (below).
-
-## WRONG - 1. Analysing the Encoder
-
-Selected because the files were all roughly compressed.
-the same amount of various different compression algorithms (Find a better quote?)
-
-Experimentation (Look for compression rate, if textual bits per character and running time):
-    Strings with structural redundancy vs Randomly Generated Strings ( / 1st million digits of PI)
-    Different window sizes
-        - Some kinda relationship between the two values?
-    Talk about various media formats containing more / less structural redundancy
-
-Might be good to mention (is for LZ78 but might impress max <3 ):
-    https://semidoc.github.io/lagarde-catastrophe
-
-## WRONG - 2. Analysing the Decoder
-
-The decoding process is like really bloody simple.
-There isn't really any part that could cause a bottleneck
-Implementationally speaking perhaps if the thing was really compresed
-and the decoder had to constantly look back on itself perhaps it could be slow?
-We could definately precompute the size of the file though and avoid any problems
-in that terms.
-
-Might have to use slightly larger file sizes to see any difference?
-Could talk about how slow it is for large files if we dont preallocate memory buffer
-but this would require an O(n) pass to compute size of entire buffer
-which for large files could get costly perhaps?
-
-## WRONG - 3. Compression Ratio
-
-Different kinds of files, files with different kind of structure
 
 ## 4. Comparison with other compression techniques
 
